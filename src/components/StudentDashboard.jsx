6cb63e31abec637407aaecdfe82d6ecfd74fb075
22CB006 <aryalakshmi.m2022csbs@sece.ac.in>
2024-11-04 15:36:36 +0530

    Student Dashboard

--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -1,11 +1,387 @@
-import React from 'react';
-import '../styles/StudentDashboard.css';
+import React from "react";
+import "../styles/StudentDashboard.css";
+import carbonEdit2 from "../assets/carbon_edit.svg";
+import carbonEdit3 from "../assets/carbon_edit.svg";
+import carbonEdit from "../assets/carbon_edit.svg";
+import formkitDown from "../assets/formkit_down.svg";
+import frame12 from "../assets/Frame sunmoon.png";
+import group5 from "../assets/Group 9.svg";
+import image from "../assets/2Danimation.jpg";
+import images1 from "../assets/images 1.png";
+import istockphoto1590114062048X20481 from "../assets/istockphoto-159011406-2048x2048 1.png";
+import letsIconsDateTodayDuotoneLine from "../assets/lets-icons_date-today-duotone-line.svg";
+import line8 from "../assets/Line 8.png";
+import line9 from "../assets/Line 9.png";
+import line10 from "../assets/Line 10.png";
+import line11 from "../assets/Line 11.png";
+import lineiconsShare2 from "../assets/lineicons_share.svg";
+import lineiconsShare from "../assets/lineicons_share.svg";
+import mynauiNotification from "../assets/mynaui_notification.svg";
+import oouiNextLtr from "../assets/ooui_next-ltr.svg";
+import rectangle1 from "../assets/Rectangle 1.png";
+import rectangle22 from "../assets/Rectangle 4.png";
+import rectangle2 from "../assets/Rectangle 2.png";
+import rectangle4 from "../assets/Rectangle 4.png";
+import rectangle5 from "../assets/Rectangle 5.png";
+import rectangle6 from "../assets/Rectangle 6.png";
+import rectangle7 from "../assets/Rectangle 4.png";
+import search from "../assets/search.svg";
+import sunSmileFaceIconKawaiiClipartIllustrationStickerVector1 from "../assets/sun-smile-face-icon-kawaii-clipart-illustration-sticker-vector 1.png";
+import uitCalender from "../assets/uit_calender.svg";
+import vector2 from "../assets/Frame 31plusyellowbg.png";
+import vector3 from "../assets/Frame 31plusyellowbg.png";
+import vector4 from "../assets/VectorPluss.svg";
+import vector5 from "../assets/VectorPluss.svg";
+import vector from "../assets/VectorPluss.svg";
 
 const StudentDashboard = () => {
   return (
-    <div className="student-dashboard">
-      <h1>Welcome to the Student Dashboard</h1>
-      <p>This is where students can access learning materials and check their progress.</p>
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="div-2" />
+
+          <div className="frame">
+            <div className="overlap-group">
+              <div className="text-wrapper">Character design</div>
+
+              <div className="text-wrapper-2">Mr. Bennett</div>
+            </div>
+
+            <div className="frame-2" />
+          </div>
+
+          <div className="group-wrapper">
+            <div className="group">
+              <img className="vector" alt="Vector" src={vector} />
+            </div>
+          </div>
+
+          <div className="frame-3">
+            <div className="overlap-2">
+              <div className="text-wrapper">Storyboarding</div>
+
+              <div className="text-wrapper-3">Ms. Caldwell</div>
+            </div>
+
+            <div className="frame-4" />
+          </div>
+
+          <div className="frame-5">
+            <div className="overlap-3">
+              <div className="text-wrapper">Visual Storytelling</div>
+
+              <div className="text-wrapper-3">Mrs. Hastings</div>
+            </div>
+
+            <div className="frame-6" />
+          </div>
+
+          <p className="p">
+            Get ahead in life is by getting started, no matter how small your
+            steps are in the beginning.
+          </p>
+
+          <div className="text-wrapper-4">Dashboard</div>
+
+          <div className="text-wrapper-5">Courses</div>
+
+          <div className="text-wrapper-6">Resources</div>
+
+          <div className="text-wrapper-7">Statistics</div>
+
+          <div className="text-wrapper-8">EDU</div>
+
+          <div className="div-wrapper">
+            <div className="text-wrapper-9">Add new board</div>
+          </div>
+
+          <img className="search" alt="Search" src={search} />
+
+          <img className="rectangle" alt="Rectangle" src={rectangle1} />
+
+          <div className="text-wrapper-10">Arya</div>
+
+          <img className="formkit-down" alt="Formkit down" src={formkitDown} />
+
+          <img className="img" alt="Frame" src={frame12} />
+
+          <p className="hi-arya-what-do-you">
+            <span className="span">
+              Hi, Arya!
+              <br /> What do you want to <br ></br>learn today?
+              <br />
+              
+            </span>
+
+           
+          </p>
+
+          <div className="frame-7">
+            <div className="text-wrapper-13">To do list</div>
+
+            <div className="frame-8">
+              <p className="create-your-own">
+                Create Your Own <br />
+                Original Character (OC)
+              </p>
+
+              <div className="frame-9">
+                <div className="text-wrapper-14">Low</div>
+              </div>
+
+              <div className="frame-10">
+                <div className="text-wrapper-15">Character Design</div>
+              </div>
+
+              <div className="text-wrapper-16">Mr. Bennett</div>
+
+              <img className="rectangle-2" alt="Rectangle" src={rectangle2} />
+            </div>
+
+            <div className="frame-11">
+              <p className="create-a-short">
+                Create a Short Storyboard
+                <br />
+                for a 1-Minute Animation
+              </p>
+
+              <div className="frame-12">
+                <div className="text-wrapper-17">High</div>
+              </div>
+
+              <div className="frame-13">
+                <div className="text-wrapper-18">Storyboarding</div>
+              </div>
+
+              <div className="text-wrapper-19">Ms. Caldwell</div>
+
+              <img className="rectangle-2" alt="Rectangle" src={rectangle22} />
+            </div>
+
+            <img className="vector-2" alt="Vector" src={image} />
+
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <img className="rectangle-3" alt="Rectangle" src={rectangle7} />
+
+                <img className="rectangle-4" alt="Rectangle" src={rectangle6} />
+
+                <img className="rectangle-5" alt="Rectangle" src={rectangle5} />
+
+                <img className="rectangle-6" alt="Rectangle" src={rectangle4} />
+
+                <div className="vector-wrapper">
+                  <img className="vector-3" alt="Vector" src={vector2} />
+                </div>
+              </div>
+            </div>
+
+            <div className="group-2">
+              <div className="text-wrapper-20">Share</div>
+
+              <img
+                className="img-2"
+                alt="Lineicons share"
+                src={lineiconsShare}
+              />
+            </div>
+
+            <div className="group-3">
+              <div className="text-wrapper-20">Edit</div>
+
+              <img className="img-2" alt="Carbon edit" src={carbonEdit} />
+            </div>
+          </div>
+
+          <div className="frame-14">
+            <div className="text-wrapper-13">My assignments</div>
+
+            <img className="vector-4" alt="Vector" src={vector3} />
+
+            <img
+              className="lineicons-share"
+              alt="Lineicons share"
+              src={lineiconsShare2}
+            />
+
+            <img className="carbon-edit" alt="Carbon edit" src={carbonEdit3} />
+
+            <div className="img-wrapper">
+              <img className="vector-5" alt="Vector" src={vector4} />
+            </div>
+
+            <div className="overlap-4">
+              <div className="frame-15" />
+
+              <div className="frame-16">
+                <div className="images-wrapper">
+                  <img className="images" alt="Images" src={images1} />
+                </div>
+
+                <div className="text-wrapper-21">3D Modeling</div>
+
+                <p className="text-wrapper-22">Create a Low-Poly 3D Asset</p>
+
+                <div className="element-mar">
+                  15
+                  <br />
+                  Mar
+                </div>
+
+                <div className="text-wrapper-23">65%</div>
+
+                <img className="group-4" alt="Group" src={group5} />
+
+                <div className="group-5">
+                  <div className="text-wrapper-24">09 Mar, 10:30 AM</div>
+
+                  <img
+                    className="lets-icons-date"
+                    alt="Lets icons date"
+                    src={letsIconsDateTodayDuotoneLine}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <img
+              className="ooui-next-ltr"
+              alt="Ooui next ltr"
+              src={oouiNextLtr}
+            />
+          </div>
+
+          <div className="frame-17">
+            <div className="overlap-5">
+              <div className="text-wrapper">2D Animation</div>
+
+              <div className="text-wrapper-3">Mr. Parker</div>
+            </div>
+
+            <div className="frame-18" />
+          </div>
+
+          <img
+            className="mynaui-notification"
+            alt="Mynaui notification"
+            src={mynauiNotification}
+          />
+        </div>
+
+        <div className="group-6">
+          <div className="text-wrapper-20">Edit</div>
+
+          <img className="img-2" alt="Carbon edit" src={carbonEdit2} />
+        </div>
+
+        <div className="frame-19">
+          <div className="text-wrapper-25">13</div>
+
+          <div className="text-wrapper-26">Monday</div>
+        </div>
+
+        <div className="frame-20">
+          <div className="text-wrapper-25">16</div>
+
+          <div className="text-wrapper-26">Thursday</div>
+        </div>
+
+        <div className="frame-21">
+          <div className="text-wrapper-25">17</div>
+
+          <div className="text-wrapper-26">Friday</div>
+        </div>
+
+        <div className="frame-22">
+          <div className="text-wrapper-27">14</div>
+
+          <div className="text-wrapper-28">Tuesday</div>
+
+          <div className="text-wrapper-29">Animation</div>
+        </div>
+
+        <div className="frame-23">
+          <div className="text-wrapper-27">15</div>
+
+          <div className="text-wrapper-28">Wednesday</div>
+
+          <div className="text-wrapper-29">3D Modeling</div>
+        </div>
+
+        <div className="text-wrapper-30">Upcoming Tests</div>
+
+        <div className="text-wrapper-31">Today’s Schedule</div>
+
+        <p className="text-wrapper-32">
+          Where brilliant ideas meet tiny canvases!
+        </p>
+
+        <div className="text-wrapper-33">Test Day</div>
+
+        <img className="uit-calender" alt="Uit calender" src={uitCalender} />
+
+        <img className="vector-6" alt="Vector" src={vector5} />
+
+        <div className="overlap-6">
+          <img
+            className="sun-smile-face-icon"
+            alt="Sun smile face icon"
+            src={sunSmileFaceIconKawaiiClipartIllustrationStickerVector1}
+          />
+
+          <div className="frame-24" />
+        </div>
+
+        <p className="text-wrapper-34">11:00 AM - 12:00 PM</p>
+
+        <p className="text-wrapper-35">01:00 PM - 02:00 PM</p>
+
+        <p className="text-wrapper-36">03:00 PM - 04:30 PM</p>
+
+        <p className="text-wrapper-37">05:00 PM - 06:00 PM</p>
+
+        <p className="text-wrapper-38">06:00 PM - 06:30 PM</p>
+
+        <div className="text-wrapper-39">2D Animation</div>
+
+        <div className="text-wrapper-40">Pencil Drawing</div>
+
+        <div className="text-wrapper-41">Storyboarding</div>
+
+        <div className="text-wrapper-42">3D Modeling</div>
+
+        <div className="text-wrapper-43">Logo Design</div>
+
+        <img className="line" alt="Line" src={line8} />
+
+        <img className="line-2" alt="Line" src={line9} />
+
+        <img className="line-3" alt="Line" src={line10} />
+
+        <img className="line-4" alt="Line" src={line11} />
+
+        <div className="istockphoto-wrapper">
+          <img
+            className="istockphoto"
+            alt="Istockphoto"
+            src={istockphoto1590114062048X20481}
+          />
+        </div>
+        
+
+        <div className="group-7">
+          <div className="frame-25" />
+
+          <div className="frame-26" />
+        </div>
+
+        <div className="group-8">
+          <div className="frame-27" />
+
+          <div className="frame-28" />
+        </div>
+      </div>
     </div>
   );
 };
